196841890db83892c8e53f326436d7a9d7043949
Ramandeep Kaur <158763564+raman-deep-kaur@users.noreply.github.com>
2025-01-19 14:34:24 +0530

    Arrays.cpp
    https://www.naukri.com/code360/problems/swap-alternate_624941?interviewProblemRedirection=true&attempt_status=COMPLETED

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,1 +1,12 @@
-
+void swapAlternate(int *arr, int size)
+{
+    //Write your code here
+    for(int i = 0; i< size; i+=2){
+        if(i+1<size){
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+}
