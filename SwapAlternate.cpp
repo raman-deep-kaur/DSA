4ab0c41a117b6d96079d7c018475b851fd46c233
Ramandeep Kaur <158763564+raman-deep-kaur@users.noreply.github.com>
2025-01-19 14:37:07 +0530

    Create SwapAlternate.cpp

---
+++ SwapAlternate.cpp
@@ -0,0 +1,14 @@
+//https://www.naukri.com/code360/problems/swap-alternate_624941?interviewProblemRedirection=true&attempt_status=COMPLETED
+
+void swapAlternate(int *arr, int size)
+{
+    //Write your code here
+    for(int i = 0; i< size; i+=2){
+        if(i+1<size){
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+}
