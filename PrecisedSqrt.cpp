c10f1d51bc7c87c2083d2253c9c23e8449f08689
Ramandeep Kaur <158763564+raman-deep-kaur@users.noreply.github.com>
2025-01-22 16:18:17 +0530

    Love Babbar (L-14)

--- PrecisedSqrt.cpp
+++ PrecisedSqrt.cpp
@@ -1,1 +1,23 @@
+int sqrtInteger(int n ){
+  //as done in sqrt.cpp
+}
+double morePrecision(int n, int precision, int tempSol){
+double factor = 1;
+double ans = tempSol;
+for(int i = 0; i <precision, i++){
+  for(int j = ans; j*j < n; j = j + factor){
+    ans = j; 
+  }
+}
+  return ans;
+}
 
+int main(){
+  int n;
+  cout<<"Enter the no."<<endl;
+  cin>>n;
+
+  int tempSol = sqrtInteger(int n );
+  cout<<"Answer is "<< morePrecision(n,3,tempSol);<<endl;
+  return 0;
+}
