6d7ef41ad57e46b897d6e385cefa58b635c0a9cf
Ramandeep Kaur <158763564+raman-deep-kaur@users.noreply.github.com>
2025-01-30 12:23:53 +0530

    LeetCode - 1047

--- RemoveAdjDup.cpp
+++ RemoveAdjDup.cpp
@@ -13,3 +13,26 @@ public:
         return s; 
     }
 };
+
+/*
+//we can also do it like this 
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string temp = "";
+        int i = 0; 
+        while(i<s.length()){
+            if(temp.empty() || s[i]!=temp.back()){
+                temp.push_back(s[i]);
+            }
+            else{
+                temp.pop_back();
+            }
+            i++;
+        } 
+        return temp;
+    }
+};
+
+*/
